d592df34c0286cd5b8cf39b0057694ce785e1d04
Heather Taylor <heather@heysparkbox.com>
2017-05-17 12:41:31 -0400

    feat: added lots of stuffs
    -- enjoy!! --

--- nobel_winners/app.js
+++ nobel_winners/app.js
@@ -1,9 +1,24 @@
+//const parse = require('csv-parse');
 const fs = require('fs');
+const csvjson = require('csv-to-json');
+
 fs.readFile('Winners.csv', function(err, data) {
-  const lines = data.toString().split(/\n/);
-  const records = lines.map(function(line) {
-    // TODO: Account for data that contains commas (wrapped in quotes)
-    return line.split(',');
-  });
-  console.log(records);
+  // const lines = data.toString().split(/\n/);
+  // const records = lines.map(function(line) {
+  //   // TODO: Account for data that contains commas (wrapped in quotes)
+  //   return line.split(',');
+  // });
+  // console.log(data.toString());
+  // const records = parse("Hello", {""}, function(records) {
+  //   console.log(records);
+  // });
+  //console.log(records);
+
+  var obj = {
+    filename: "Winners.csv"
+  };
+  var callback = function(err, json) {
+    console.log(json[0].Winner);
+  };
+  csvjson.parse(obj, callback);
 });
